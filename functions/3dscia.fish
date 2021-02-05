3fd4fea432be7ceeb7ae918a239e990876055588
Stefano Pigozzi <me@steffo.eu>
2021-02-05 02:33:41 +0100

    Add a few new functions

---
+++ functions/3dscia.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function 3dscia --wraps='3dsconv --boot9=/mnt/tera/ext4/games/emulation/3ds/firmware/CEM10206672_boot9_00.bin' --description 'alias 3dscia 3dsconv --boot9=/mnt/tera/ext4/games/emulation/3ds/firmware/CEM10206672_boot9_00.bin'
+  3dsconv --boot9=/mnt/tera/ext4/games/emulation/3ds/firmware/CEM10206672_boot9_00.bin $argv;
+end
---
+++ functions/idea.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function idea --wraps='idea -e -w' --description 'alias idea idea -e -w'
+ command idea -e -w $argv;
+end
