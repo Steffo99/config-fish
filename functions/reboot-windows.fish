8d553e8fd93405996b22bfa8669fd9a61eb473cb
Stefano Pigozzi <me@steffo.eu>
2024-05-20 02:04:26 +0200

    Remove broken reboot-windows command

--- functions/reboot-windows.fish
+++
@@ -1,3 +0,0 @@
-function reboot-windows --wraps='systemctl reboot --boot-loader-entry=windows.conf' --description 'alias reboot-windows systemctl reboot --boot-loader-entry=windows.conf'
-  systemctl reboot --boot-loader-entry=windows.conf $argv; 
-end
