6c4f17a5a4c5897526551ff0ba9c61db901f529c
Stefano Pigozzi <me@steffo.eu>
2024-09-03 01:34:34 +0200

    `ll`: Group directories first

--- functions/ll.fish
+++ functions/ll.fish
@@ -2,6 +2,6 @@ which ls --skip-functions >/dev/null 2>/dev/null
 if [ $status -eq 0 ]
     function ll --wraps=ls
 		log-t "" "Listing" "all files in the directory..."
-        command ls --all --author --color --kibibytes --human-readable -l $argv;
+        command ls --all --author --color --kibibytes --human-readable --group-directories-first -l $argv;
     end
 end
