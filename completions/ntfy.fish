5d4cf5202f109e68eba9ff010e34487860f17189
Stefano Pigozzi <me@steffo.eu>
2024-09-26 06:43:25 +0200

    fix

--- completions/ntfy.fish
+++ completions/ntfy.fish
@@ -10,12 +10,21 @@ complete \
 	--long-option="title" \
 	--description="Set title"
 
+function __ntfy_complete_priorities
+	echo max \t 'Really long vibration bursts, default notification sound with a pop-over notification.'
+	echo high \t 'Long vibration burst, default notification sound with a pop-over notification.'
+	echo default \t 'Short default vibration and sound. Default notification behavior.'
+	echo low \t 'No vibration or sound. Notification will not visibly show up until notification drawer is pulled down.'
+	echo min \t 'No vibration or sound. The notification will be under the fold in Other notifications.'
+end
+
 complete \
 	--command='ntfy' \
 	--short-option="p" \
 	--long-option="priority" \
-	--description="Set priority" \ 
-	--arguments='urgent high default low min'
+	--description="Set priority" \
+	--no-files \
+	--arguments='(__ntfy_complete_priorities)'
 
 complete \
 	--command='ntfy' \
