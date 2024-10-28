7f85c461b96af0acd810ee66baa4fa6fec2262f9
Stefano Pigozzi <me@steffo.eu>
2024-10-28 12:41:57 +0100

    Remove unnecessary path additions

--- path/cargo.fish
+++
@@ -1,5 +0,0 @@
-which cargo --skip-functions >/dev/null 2>/dev/null
-if [ $status -eq 0 ]
-    log-t "Adding" "cargo" "binaries to the PATH..."
-    fish_add_path ~"/.cargo/bin"
-end
--- path/uv.fish
+++
@@ -1,5 +0,0 @@
-which uv --skip-functions >/dev/null 2>/dev/null
-if [ $status -eq 0 ]
-    log-t "Adding" "uv" "binaries to the PATH..."
-    fish_add_path ~"/.local/bin"
-end
