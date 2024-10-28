cb6235c606aef169ec8fe072ca4330ecdbb7b5f9
Stefano Pigozzi <me@steffo.eu>
2024-10-28 12:40:50 +0100

    Fix something in emoji generation

--- functions/gen-emoji-telegram.fish
+++ functions/gen-emoji-telegram.fish
@@ -4,7 +4,7 @@ if [ $status -eq 0 ]
         log-t "Creating" "Telegram" "directory..."
         command mkdir --parents "Telegram"
         log-d "Processing" "$argv[1]" "for Telegram use..."
-        command magick -background none "$argv[1]" -resize "100x100^" -gravity center -extent 100x100 $argv[2..-1] "Telegram/$argv[1]"
+        command magick -background none "$argv[1]"'[100x100]' -gravity 'center' -extent '100x100' $argv[2..-1] "Telegram/$argv[1]"
         log-t "Done! Emoji added to the" "Telegram" "directory!"
     end
 end
